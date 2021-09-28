5da3d4c1b7cc32c1e6ca57bd5515f8419059c36f
srinidhis <srinidhi.s@razorpay.com>
2021-09-28 14:19:50 +0530

    feat: adds README for all the hooks

--- hooks/sqs_configurator/constants/constants.go
+++ hooks/sqs_configurator/constants/constants.go
@@ -1,8 +1,8 @@
 package constants
 
 const (
-	LOCALSTACK_ENDPOINT = "https://localstack-services.dev.razorpay.in"
-	LOCALSTACK_PREFIX   = "https://localstack-services.dev.razorpay.in/000000000000"
+	LOCALSTACK_ENDPOINT = "https://localstack-services.dev.com"
+	LOCALSTACK_PREFIX   = "https://localstack-services.dev.razorpay.com/000000000000"
 	AWS_ENDPOINT        = "https://sqs.ap-south-1.amazonaws.com"
 	AWS_PREFIX          = "https://sqs.ap-south-1.amazonaws.com/000000000000"
 	AWS_REGION          = "ap-south-1"
